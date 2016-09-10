fa56654956bebeb38321b260a75a86d6248654c7
quant4s <qiu13305278179@189.cn>
2016-09-10 09:29:53 +0800

    修改了默认平滑因子的计算BUG

--- src/test/scala/quanter/indicators/ExponentialMovingAverageSpec.scala
+++ src/test/scala/quanter/indicators/ExponentialMovingAverageSpec.scala
@@ -13,7 +13,7 @@ class ExponentialMovingAverageSpec extends QuanterUnitSpec {
   describe ("用外部文件测试") {
     it("spy_with_indicators.txt EMA14 列") {
       val ema = new ExponentialMovingAverage(14)
-      TestHelper.testIndicator(ema, "EMA14", 0.025)
+      TestHelper.testIndicator(ema, "EMA14", 0.01)
     }
   }
 }
