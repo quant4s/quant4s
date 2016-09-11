b63eef4f6b68bfdf92aa05c36b5a4fdf2f9facc1
quant4s <qiu13305278179@189.cn>
2016-09-11 17:52:22 +0800

    完成ATR 测试案例

--- src/test/scala/quanter/indicators/AverageTrueRangeSpec.scala
+++ src/test/scala/quanter/indicators/AverageTrueRangeSpec.scala
@@ -3,7 +3,10 @@
   */
 package quanter.indicators
 
+import java.util.Date
+
 import quanter.QuanterUnitSpec
+import quanter.data.market.TradeBar
 
 /**
   *
@@ -13,6 +16,43 @@ class AverageTrueRangeSpec extends QuanterUnitSpec {
     it("spy_atr.txt ATR14 列") {
       val atr = new AverageTrueRange(14, MovingAverageType.Simple)
       TestHelper.testTradeBarIndicator(atr, "spy_atr.txt", "ATR14")
+    }
+  }
+
+  describe("测试ATR 指标 属性") {
+    it("Reset 后 ATR 应该为默认状态") {
+      val atr = new AverageTrueRange(14, MovingAverageType.Simple)
+
+      val bar = new TradeBar()
+      bar.time = new Date()
+      bar.open = 1.0
+      bar.high = 3.0
+      bar.low = 0.5
+      bar.close = 2.75
+      bar.volume = 1234567890
+      atr.update(bar)
+
+      atr.reset
+
+      TestHelper.assertIndicatorIsInDefaultState(atr)
+      TestHelper.assertIndicatorIsInDefaultState(atr.trueRange)
+    }
+    it("TrueRangePropertyIsReadyAfterOneSample") {
+      val atr = new AverageTrueRange(14, MovingAverageType.Simple)
+
+      atr.trueRange.isReady should be(false)
+
+      val bar = new TradeBar()
+      bar.time = new Date()
+      bar.open = 1.0
+      bar.high = 3.0
+      bar.low = 0.5
+      bar.close = 2.75
+      bar.volume = 1234567890
+      atr.update(bar)
+
+      atr.trueRange.isReady should be(true)
+
     }
   }
 }
