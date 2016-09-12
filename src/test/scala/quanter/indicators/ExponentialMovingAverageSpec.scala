11dd27647797239bf15a11f95a9ee9caf09d4dc1
quant4s <qiu13305278179@189.cn>
2016-09-12 08:04:55 +0800

    增加了测试 案例

--- src/test/scala/quanter/indicators/ExponentialMovingAverageSpec.scala
+++ src/test/scala/quanter/indicators/ExponentialMovingAverageSpec.scala
@@ -3,14 +3,36 @@
   */
 package quanter.indicators
 
+import java.util.Date
+
 import quanter.QuanterUnitSpec
 import quanter.indicators.window.ExponentialMovingAverage
+import quanter.CommonExtensions._
+import quanter.indicators.IndicatorExtensions._
 
 /**
   *
   */
 class ExponentialMovingAverageSpec extends QuanterUnitSpec {
   epsilon = 0.01
+  describe("测试指标是否正确") {
+    it("测试4周期的数据") {
+      val period = 4
+      val values: Array[Double] = Array(1.0, 10.0, 100.0, 1000.0)
+      val expFactor = 2.0/(1.0 + period)
+
+      val ema4 = new ExponentialMovingAverage(period)
+
+      var current = 0.0
+      for(i <- 0 until values.length) {
+        ema4.update(new IndicatorDataPoint(new Date().addSeconds(i), values(i)))
+        current = if(i == 0) values(i)
+        else values(i) * expFactor + (1-expFactor) * current
+          ema4.current.value should be(current +- 0.001)
+      }
+
+    }
+  }
   describe ("用外部文件测试 EMA") {
     it("spy_with_indicators.txt EMA14 列") {
       val ema = new ExponentialMovingAverage(14)
