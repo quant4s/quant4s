2707e1f59c6fdc65f465562316d217a3f4ef4466
quant4s <qiu13305278179@189.cn>
2016-09-27 07:45:26 +0800

    删除一个测试文件

--- src/test/scala/quanter/rest/OrderServiceSpec.scala
+++ src/test/scala/quanter/rest/OrderServiceSpec.scala
@@ -8,22 +8,22 @@ import spray.http.{HttpEntity, MediaTypes}
 /**
   *
   */
-class OrderServiceSpec  extends RoutingSpec with OrderService{
+class OrderServiceSpec  extends RoutingSpec with StrategyService{
   implicit def actorRefFactory = system
 
   "订单提交," - {
     " 提交数据订阅请求" in {
       Post("/order", HttpEntity(MediaTypes.`application/json`,
         """{"strategyId": 1,"orders":[{"orderNo":1,"quantity":1000,"symbol":"000001.XSHE","orderType":0,"openClose": "O","orderStatus":0,"side": 1, "tradeAccountId":999}]}"""
-      )) ~> orderServiceRoute ~> check {
+      )) ~> strategyServiceRoute ~> check {
         responseAs[String] shouldEqual """{"code":0}"""
       }
       Post("/order", HttpEntity(MediaTypes.`application/json`,
         """{"strategyId": 1,"orders":[{"orderNo":2,"quantity":1000,"symbol":"000001.XSHE","orderType":0,"openClose": "O","orderStatus":0,"side": 1, "tradeAccountId":999}]}"""
-      )) ~> orderServiceRoute ~> check {
+      )) ~> strategyServiceRoute ~> check {
         responseAs[String] shouldEqual """{"code":0}"""
       }
-      Delete("/order/1-1") ~> orderServiceRoute ~> check {
+      Delete("/order/1-1") ~> strategyServiceRoute ~> check {
         responseAs[String] shouldEqual """{"code":0}"""
       }
 
