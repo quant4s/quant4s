e4ea9bb30b9da370938139b265458e8a59ef6af3
quant4s <qiu13305278179@189.cn>
2016-10-09 08:08:11 +0800

    增加了OBV 指标

---
+++ src/main/scala/quanter/indicators/OnBalanceVolume.scala
@@ -0,0 +1,51 @@
+/**
+  *
+  */
+package quanter.indicators
+
+import quanter.data.market.TradeBar
+
+/**
+  *
+  */
+class OnBalanceVolume(ppname: String) extends TradeBarIndicator(ppname) {
+
+  var _previousInput: TradeBar = null
+
+  override def isReady: Boolean = _previousInput != null
+
+  override def reset: Unit = {
+    _previousInput = null
+    super.reset
+  }
+
+  override def toJson: String = "{\"symbol\":%s,\"obv\":%f, \"time\":%d}"
+    .format(symbol, current.value, time.getTime())
+
+  override def computeNextValue(input: TradeBar): Double = {
+    var obv = current.value
+
+    if (_previousInput != null)
+    {
+      if (input.value > _previousInput.value)
+      {
+        obv += input.volume
+        update(input)
+      }
+      else if (input.value < _previousInput.value)
+      {
+        obv -= input.volume
+        update(input)
+      }
+    }
+    else
+    {
+      obv = input.volume
+      update(input)
+    }
+
+    _previousInput = input
+
+    obv
+  }
+}
