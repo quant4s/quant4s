bc12662d839f1906c24b09522e0646510c57248d
quant4s <qiu13305278179@189.cn>
2016-10-25 21:34:07 +0800

    增加了volume指标

--- src/main/scala/quanter/indicators/Volume.scala
+++ src/main/scala/quanter/indicators/Volume.scala
@@ -17,7 +17,13 @@ class Volume(pname: String, pperiod: Int, epsilon: Double = 0.2) extends WindowI
 
   override protected def computeNextValue(window: ReadOnlyWindow[TradeBar], input: TradeBar): Double = {
     if(isReady) {
-      val vol = window.reduce((a,b) => a.volume + b.volume)
+//      val vol: Long = window.reduce((a,b) => a.volume + b.volume)
+      var vol: Long = 0
+      val sum:(TradeBar => Double) = (a => {
+        vol += a.volume
+        vol
+      })
+      window.foreach(sum)
       if(((vol / window.size) * (1+epsilon)).toLong < input.volume) 1
       else if(((vol / window.size) * (1-epsilon)).toLong > input.volume) -1
       else 0
